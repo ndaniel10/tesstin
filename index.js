8376fdd74bcd02abd8874becaa93ca5107e79862
ndaniel10 <noahdaniel10@gmail.com>
2018-11-30 20:52:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,32 +7,6 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-if(message.content === ':kick') {
-    //!kick @daeshan askin for it
-
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("SLAVES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("NGOLO KANYE ROYAL")) return message.channel.send("That person can't be kicked!");
-
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "incidents");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
-
-    return;
-  }
 
 //general and messages
 
