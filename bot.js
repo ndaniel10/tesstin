a921c2b1319d86dc2d0ed2bf4908f271c2b82188
ndaniel10 <noahdaniel10@gmail.com>
2018-11-30 12:11:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
+const Discord = require ('discord.js');
+const bot = new Discord.Client ();
 const { Client, Attachment } = require('discord.js');
 //messages
 
 
-client.on('message', (message) =>{
+bot.on('message', (message) =>{
     if(message.content.startsWith(':a')) {
         message.channel.send('Shut up you`re gay')
     
@@ -14,7 +13,7 @@ client.on('message', (message) =>{
     
 });
 
-client.on('message', (message) =>{
+bot.on('message', (message) =>{
     if(message.content.startsWith(':f')) {
         message.channel.send('If I met you in real life I would give you a concussion')
     
@@ -23,59 +22,30 @@ client.on('message', (message) =>{
     
 });
 
-client.on('message', (message) => {
+bot.on('message', (message) => {
     if(message.content.startsWith (':rank')) {
       message.reply('https://cdn.discordapp.com/attachments/489843725007192074/517043007690899485/unknown.png');
     }
   });
   
-client.on('message', (message) =>{
+  bot.on('message', (message) =>{
     if (message.content === 'Sergio Ramos')  {
         const attachment = new Attachment('http://i37.tinypic.com/ok0pqd.jpg');
         message.channel.send(attachment);
     }
 });
   
-client.on('message', (message) =>{
+bot.on('message', (message) =>{
     if(message.content.startsWith(':help')) {
       message.reply('**To start the simulation you must state with the appropriate spacing or else it wont work properly. ** ```:AndresSim <emoji> <ClubName> <emoji> <ClubName> <Competition Name> <Stadium Name> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100>``` For an example type :example');
     }
 });
 
-client.on('message', (message) =>{
+bot.on('message', (message) =>{
     if(message.content.startsWith(':example')) {
         const attachment = new Attachment('https://cdn.discordapp.com/attachments/489843725007192074/514894613157511168/unknown.png');
         message.channel.send(attachment);
     }
-});
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'Sergio Ramos') {
-
-       message.reply('http://i37.tinypic.com/ok0pqd.jpg');
-
-       }
-
 });
 
  
