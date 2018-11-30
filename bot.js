dcc2c1434bdaaa390dac0a73f9049cb553c11fd1
ndaniel10 <noahdaniel10@gmail.com>
2018-11-30 08:11:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,45 +22,53 @@ client.on('message', message => {
 
 });
 
-client.on('message', (message) =>{
-    if(message.content.startsWith(':a')) {
-        message.channel.send('Shut up you`re gay')
-    
-        message.delete(message)
-    } 
- 
-client.on('message', (message) =>{
-    if(message.content.startsWith(':f')) {
-        message.channel.send('If I met you in real life I would give you a concussion')
-    
-        message.delete(message)
-    } 
-    
-});
+client.on('message', message => {
 
-client.on('message', (message) => {
-    if(message.content.startsWith (':rank')) {
-      message.reply('https://cdn.discordapp.com/attachments/489843725007192074/517043007690899485/unknown.png');
-    }
-  });
-  
-client.on('message', (message) =>{
-    if (message.content === 'Sergio Ramos')  {
+    if (message.content === ':a') {
+
+       message.reply('Shut up you gay boy');
+       message.delete(message)
+
+       }
+ });
+
+client.on('message', message => {
+
+    if (message.content === ':f') {
+
+       message.reply('If I met you in real life I would give you a concussion');
+       message.delete(message)
+
+       }
+ });
+
+client.on('message', message => {
+
+    if (message.content === 'Sergio Ramos') {
+
        message.reply('http://i37.tinypic.com/ok0pqd.jpg');
-    }
-});
-  
-client.on('message', (message) =>{
-    if(message.content.startsWith(':help')) {
-      message.reply('**To start the simulation you must state with the appropriate spacing or else it wont work properly. ** ```:AndresSim <emoji> <ClubName> <emoji> <ClubName> <Competition Name> <Stadium Name> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100>``` For an example type :example');
-    }
-});
 
-client.on('message', (message) =>{
-    if(message.content.startsWith(':example')) {
-         message.reply('https://cdn.discordapp.com/attachments/489843725007192074/514894613157511168/unknown.png')
-    }
+       }
+ });
 
+client.on('message', message => {
+
+    if (message.content === ':help') {
+
+       message.reply('**To start the simulation you must state with the appropriate spacing or else it wont work properly. ** ```:AndresSim <emoji> <ClubName> <emoji> <ClubName> <Competition Name> <Stadium Name> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100>``` For an example type :example');
+     
+
+       }
+ });
+
+
+client.on('message', message => {
+
+    if (message.content === ':example') {
+
+       message.reply('https://cdn.discordapp.com/attachments/489843725007192074/514894613157511168/unknown.png');
+
+       }
     
 });
 });
