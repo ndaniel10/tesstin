d0f81dddf0bdc7ee4ee957fcd1a1abea8086ce21
ndaniel10 <noahdaniel10@gmail.com>
2018-11-30 16:52:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Discord = require ('discord.js');
-const bot = new Discord.Client ();
+const client = new Discord.Client ();
 const { Client, Attachment } = require('discord.js');
 //messages
 
 
-bot.on('message', (message) =>{
+client.on('message', (message) =>{
     if(message.content.startsWith(':a')) {
         message.channel.send('Shut up you`re gay')
     
@@ -13,7 +13,7 @@ bot.on('message', (message) =>{
     
 });
 
-bot.on('message', (message) =>{
+client.on('message', (message) =>{
     if(message.content.startsWith(':f')) {
         message.channel.send('If I met you in real life I would give you a concussion')
     
@@ -22,26 +22,26 @@ bot.on('message', (message) =>{
     
 });
 
-bot.on('message', (message) => {
+client.on('message', (message) => {
     if(message.content.startsWith (':rank')) {
       message.reply('https://cdn.discordapp.com/attachments/489843725007192074/517043007690899485/unknown.png');
     }
   });
   
-  bot.on('message', (message) =>{
+  client.on('message', (message) =>{
     if (message.content === 'Sergio Ramos')  {
         const attachment = new Attachment('http://i37.tinypic.com/ok0pqd.jpg');
         message.channel.send(attachment);
     }
 });
   
-bot.on('message', (message) =>{
+client.on('message', (message) =>{
     if(message.content.startsWith(':help')) {
       message.reply('**To start the simulation you must state with the appropriate spacing or else it wont work properly. ** ```:AndresSim <emoji> <ClubName> <emoji> <ClubName> <Competition Name> <Stadium Name> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100>``` For an example type :example');
     }
 });
 
-bot.on('message', (message) =>{
+client.on('message', (message) =>{
     if(message.content.startsWith(':example')) {
         const attachment = new Attachment('https://cdn.discordapp.com/attachments/489843725007192074/514894613157511168/unknown.png');
         message.channel.send(attachment);
@@ -49,7 +49,7 @@ bot.on('message', (message) =>{
 });
 
 //simulator
-bot.on('message', (message) =>{
+client.on('message', (message) =>{
     if(message.content.startsWith(':AndresSim')) {
         var messagesplit = message.content.split(" ")
         var P1Goals = 0
