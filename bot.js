1586836dffb4bb097e6d941040ba9d4e5a22bc52
ndaniel10 <noahdaniel10@gmail.com>
2018-11-30 12:08:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,53 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+const { Client, Attachment } = require('discord.js');
+//messages
 
+
+client.on('message', (message) =>{
+    if(message.content.startsWith(':a')) {
+        message.channel.send('Shut up you`re gay')
+    
+        message.delete(message)
+    } 
+    
+});
+
+client.on('message', (message) =>{
+    if(message.content.startsWith(':f')) {
+        message.channel.send('If I met you in real life I would give you a concussion')
+    
+        message.delete(message)
+    } 
+    
+});
+
+client.on('message', (message) => {
+    if(message.content.startsWith (':rank')) {
+      message.reply('https://cdn.discordapp.com/attachments/489843725007192074/517043007690899485/unknown.png');
+    }
+  });
+  
+client.on('message', (message) =>{
+    if (message.content === 'Sergio Ramos')  {
+        const attachment = new Attachment('http://i37.tinypic.com/ok0pqd.jpg');
+        message.channel.send(attachment);
+    }
+});
+  
+client.on('message', (message) =>{
+    if(message.content.startsWith(':help')) {
+      message.reply('**To start the simulation you must state with the appropriate spacing or else it wont work properly. ** ```:AndresSim <emoji> <ClubName> <emoji> <ClubName> <Competition Name> <Stadium Name> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100> <Player1> <1-100> <Player2> <1-100> <Player3> <1-100>``` For an example type :example');
+    }
+});
+
+client.on('message', (message) =>{
+    if(message.content.startsWith(':example')) {
+        const attachment = new Attachment('https://cdn.discordapp.com/attachments/489843725007192074/514894613157511168/unknown.png');
+        message.channel.send(attachment);
+    }
+});
  
 
 client.on('ready', () => {
