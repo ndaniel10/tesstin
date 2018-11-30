8ffcb3f0f52767199fef6df03bf79f2ed0ab6771
ndaniel10 <noahdaniel10@gmail.com>
2018-11-30 20:21:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,36 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+if (message.content === ':ban')  {
+  let bUser = message.guild.member(message.mentions.members.first() || message.guild.members.get(args[0]));
+  if (!bUser) return message.channel.send("Can't find user!");
+  let bReason = args.join(" ").slice(22);
+  if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+  if (bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
+
+  let banEmbed = new Discord.RichEmbed()
+    .setDescription("Ban Management")
+    .setColor("#bc0000")
+    .addField("Banned User", `${bUser.user.tag} with ID ${bUser.id}`)
+    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Banned In", message.channel.name)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
+
+  let incidentchannel = message.guild.channels.find(`name`, "incidents");
+  if (!incidentchannel) return message.channel.send("Can't find incidents channel.");
+
+  message.guild.member(bUser).ban({
+    reason: bReason
+  });
+  message.delete();
+  incidentchannel.send({
+    embed: banEmbed
+  });
+
+  return;
+}
+
 //general and messages
 
 client.on('message', (message) =>{
