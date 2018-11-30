134beff14dc93158e393bfd3d2b5e6711b72c847
ndaniel10 <noahdaniel10@gmail.com>
2018-12-01 00:20:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,46 +8,6 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-module.exports.run = async (bot, message, args) => {
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  if (message.content.startsWith(':kick')) {
-    const user = message.mentions.users.first();
-    if (user) {
-      const member = message.guild.member(user);
-      if (member) {
-       
-        member.kick('Optional reason that will display in the audit logs').then(() => {
-          message.reply(`Successfully kicked ${user.tag}`);
-        }).catch(err => {
-         
-          message.reply('I was unable to kick the member');
-          console.error(err);
-          
-        });
-      } else {
-        message.reply('That user isn\'t in this guild!');
-      }
-
-    } else {
-      message.reply('You didn\'t mention the user to kick!');
-    }
-  }
-});
-
-
-
-
-
-
 //general and messages
 
 client.on('message', (message) =>{
