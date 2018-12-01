89ab8733c99b3b2c06e7f1b9d176b6861e74151d
ndaniel10 <noahdaniel10@gmail.com>
2018-12-01 18:54:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ const Discord = require ('discord.js');
 const client = new Discord.Client ();
 const { Client, Attachment } = require('discord.js');
 
+//moderations
+if (member.hasPermission('KICK_MEMBERS')) {
+    console.log('This member can kick');
+     // Ignore messages that aren't from a guild
+  if (!message.guild) return;
 
+  // If the message content starts with "!kick"
+  if (message.content.startsWith(':kick')) {
+    
+    const user = message.mentions.users.first();
+  
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+       
+        member.kick('Optional reason that will display in the audit logs').then(() => {
+          message.reply(`Successfully kicked ${user.tag}`);
+        }).catch(err => {
+         
+          message.reply('I was unable to kick the member');
+          console.error(err);
+        });
+      } else {
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+      message.reply('You didn\'t mention the user to kick!');
+    }
+  }
+}
+ 
 
 client.on('ready', () => {
   console.log('I am ready!');
