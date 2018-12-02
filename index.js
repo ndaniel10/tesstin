ca0c771735f316140574503e2798cd6f21c5c3d3
ndaniel10 <noahdaniel10@gmail.com>
2018-12-02 01:41:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 });
 
 //moderations
-client.on("message", async message => {
+client.on('message', (message) => {
   // This event will run on every single message received, from any channel or DM.
   
   // It's good practice to ignore other bots. This also makes your bot ignore itself
