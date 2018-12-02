a8132eb51eb0012c60ff3d9b79c017490482eb21
ndaniel10 <noahdaniel10@gmail.com>
2018-12-02 11:18:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require ('discord.js');
 const client = new Discord.Client ();
 const { Client, Attachment } = require('discord.js');
 
+  bot.user.setActivity("Minecraft Porn™", {type: "WATCHING"});
+
 
 //moderations
 
